a6c6a3791e36621be1c275a83eddc45a6fcccc15
Hein <heinthetaung.cloud@gmail.com>
2021-07-27 14:16:36 +0800

    create two modals - login and register instead of using tabpanel

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import Header from '../common/header/Header';
 import Login from './login/Login';
+import Register from './register/Register';
+
 
 
 let Controller = () => {
 
     const [showLogin, setShowLogin] = useState(false)
+    const [showRegister, setShowRegister] = useState(false)
+    const [registerTab, setregisterTab] = React.useState(1);
+
+
+    const registerTabChangeHandler = (event, newValue) => {
+        setregisterTab(newValue);
+    };
+
     let loginHandler = () => {
         console.log('login clicked')
         setShowLogin(true)
+        setShowRegister(false)
     }
 
     let logoutHandler = () => {
@@ -17,11 +28,24 @@ let Controller = () => {
 
     let registerHandler = () => {
         console.log('register clicked')
+        setShowLogin(false)
+        setShowRegister(true)
     }
 
     let loginCloseHandler = () => {
         console.log('login modal closed')
         setShowLogin(false)
+    }
+
+    let registerCloseHandler = () => {
+        console.log('register modal closed')
+        setShowRegister(false)
+    }
+
+    let closeHandler = () => {
+        console.log('login/register modal closed')
+        setShowLogin(false)
+        setShowRegister(false)
     }
 
     return (
@@ -31,7 +55,12 @@ let Controller = () => {
                 loginAction={loginHandler}
                 logoutAction={logoutHandler}
                 registerAction={registerHandler}></Header>
-            <Login open={showLogin} closeHandler={loginCloseHandler}></Login>
+            <Login open={showLogin}
+                closeHandler={closeHandler}
+                tabChangeHandler={registerHandler}></Login>
+            <Register open={showRegister}
+                closeHandler={closeHandler}
+                tabChangeHandler={loginHandler}></Register>
         </div>
 
     )
--- src/screens/register/Register.js
+++ src/screens/register/Register.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './Login.css'
+import './Register.css'
 import Modal from 'react-modal';
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
-import AppBar from "@material-ui/core/AppBar";
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from "@material-ui/core/Button";
@@ -35,6 +34,12 @@ export default function Login(props) {
         };
     }
 
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const [value, setValue] = React.useState(1);
+
     return (
         <div>
             <Modal className='modal-container'
@@ -42,7 +47,7 @@ export default function Login(props) {
                 onRequestClose={props.closeHandler}
                 appElement={root}>
                 <Tabs
-                    value={0}
+                    value={value}
                     onChange={props.tabChangeHandler}
                     variant="fullWidth"
                     aria-label="simple tabs example">
@@ -51,18 +56,39 @@ export default function Login(props) {
                 </Tabs>
                 <div className='text-input'>
                     <FormControl>
-                        <InputLabel htmlFor="my-input">Username</InputLabel>
-                        <Input id="username-input" aria-describedby="my-helper-text" />
+                        <InputLabel htmlFor="my-input">First Name*</InputLabel>
+                        <Input id="first-input" aria-describedby="my-helper-text" />
                     </FormControl>
                 </div>
                 <div className='text-input'>
                     <FormControl>
-                        <InputLabel htmlFor="my-input">Password</InputLabel>
+                        <InputLabel htmlFor="my-input">Last Name*</InputLabel>
+                        <Input id="last-input" aria-describedby="my-helper-text" />
+                    </FormControl>
+                </div>
+                <div className='text-input'>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Email*</InputLabel>
+                        <Input id="email-input" aria-describedby="my-helper-text" />
+                    </FormControl>
+                </div>
+                <div className='text-input'>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Password*</InputLabel>
                         <Input id="password-input" aria-describedby="my-helper-text" />
                     </FormControl>
                 </div>
+                <div className='text-input'>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Contact Number.*</InputLabel>
+                        <Input id="phone-input" />
+                    </FormControl>
+                </div>
+                <div id='login-status'>
+                    login successful
+                </div>
                 <div className='button'>
-                    <Button variant="contained" color='primary'>Login</Button>
+                    <Button variant="contained" color='primary'>Register</Button>
                 </div>
             </Modal>
         </div>
