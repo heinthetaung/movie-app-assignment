6a2967ad2f8729c0b494b8a368f59dac5f40678a
Hein <heinthetaung.cloud@gmail.com>
2021-08-07 21:06:03 +0800

    add image on the left panel

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../../common/header/Header'
 import { useLocation, useParams, useHistory } from 'react-router-dom';
 import Typography from "@material-ui/core/Typography";
 import './Details.css'
+import { useState } from 'react';
 
 const Details = () => {
 
@@ -10,6 +11,8 @@ const Details = () => {
     let params = useParams();
     console.log(location.pathname)
     console.log(params)
+
+    const [movie, setMovie] = useState({})
 
     let history = useHistory();
 
@@ -22,8 +25,35 @@ const Details = () => {
         height: '24px'
     }
 
-    return (
+    let fetchData = async (baseURL, parameter = '') => {
+        const url = baseURL + parameter
+        console.log(url)
+        try {
+            let rawResponse = await fetch(url, {
+                mode: 'cors',
+                headers: { "Accept": "application/json;charset=UTF-8" }
+            })
+            if (rawResponse.ok) {
+                return await rawResponse.json();
+            }
+        } catch (e) {
+            console.error(e)
+        }
 
+    }
+
+    useEffect(() => {
+        let baseURL = 'http://localhost:8085/api/v1/movies/'
+        fetchData(baseURL, params.movie_id).then(
+            (data) => {
+                if (data !== undefined) setMovie(data)
+
+            }
+        )
+    }, [])
+    console.log(movie['poster_url'])
+
+    return (
         <div>
             <Header></Header>
             <Typography style={homeButtonStyle} onClick={history.goBack}>
@@ -32,6 +62,7 @@ const Details = () => {
             <div className='main-container'>
                 <div className='left'>
                     left
+                    <img src={movie['poster_url']} alt='poster' />
                 </div>
                 <div className='center'>
                     center
